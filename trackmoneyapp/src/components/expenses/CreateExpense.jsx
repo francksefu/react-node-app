08b33b700ec59785730a5bb0c782401ce08df7c3
francksefu <francksefu1998@gmail.com>
2025-02-17 10:30:06 +0200

    [FIX] Fix bug related to date input, witch refuse to show up on update expenses

--- trackmoneyapp/src/components/expenses/CreateExpense.jsx
+++ trackmoneyapp/src/components/expenses/CreateExpense.jsx
@@ -3,7 +3,7 @@ import ExpensesContext from "../../context/expenses";
 
 const CreateExpense = ({closeModal ,editExpenseItem = null}) => {
     const [id, setId] = useState(editExpenseItem ? editExpenseItem.id : null);
-    const [date, setDate] = useState(editExpenseItem ? editExpenseItem.date : new Date());
+    const [dateT, setDate] = useState(editExpenseItem ? editExpenseItem.date.slice(0, 16) : new Date().toISOString().slice(0, 16));
     const [amount, setAmount] = useState(editExpenseItem ? editExpenseItem.amount : 0);
     const [description, setDescription] = useState(editExpenseItem ? editExpenseItem.description : '');
     const [idCategorie, setIdCategorie] = useState(editExpenseItem ? editExpenseItem.idCategorie : '1');
@@ -12,11 +12,10 @@ const CreateExpense = ({closeModal ,editExpenseItem = null}) => {
     
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log({date, amount, description, idCategorie});
         if (editExpenseItem) {
-            changeExpense(amount, date, description, idCategorie, id);
+            changeExpense(amount, dateT, description, idCategorie, id);
         } else {
-            createExpense(date, amount, description, idCategorie );
+            createExpense(dateT, amount, description, idCategorie );
         }
         closeModal();
     }
@@ -27,11 +26,11 @@ const CreateExpense = ({closeModal ,editExpenseItem = null}) => {
             <div className="mb-5">
                 <label htmlFor="date" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Date</label>
                 <input
-                    type="date"
+                    type="datetime-local"
                     id="date"
                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     required
-                    value={date}
+                    value={dateT}
                     onChange={(e) => setDate(e.target.value)}
                 />
                 <label htmlFor="amount" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Amount</label>
--- trackmoneyapp/src/components/expenses/ExpensesList.jsx
+++ trackmoneyapp/src/components/expenses/ExpensesList.jsx
@@ -42,12 +42,6 @@ const ExpensesList = () => {
           transform: 'translate(-50%, -50%)',
         },
       };
-    
-      
-    
-      function closeModalLoad() {
-        setIsOpen(false);
-      }
 
     //
 
@@ -81,7 +75,7 @@ const ExpensesList = () => {
 
     return(
         <>
-            <button className=" mx-auto m-3 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={openModal}>Open Modal</button>
+            <button className=" mx-auto m-3 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={openModal}>Create new Expenses</button>
             <div className="p-6 px-0 overflow-scroll">
                 <table className="w-full text-left table-auto min-w-max">
                 <thead>
