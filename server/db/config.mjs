9539ac79fd63bfab30503212bbc4a7c19059e302
francksefu <francksefu1998@gmail.com>
2025-06-03 13:44:10 +0200

    Deployement

--- server/db/config.mjs
+++ server/db/config.mjs
@@ -1,11 +1,11 @@
 import mysql from "mysql";
 
 const connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'root',
-  port : '8889',
-  database : 'testmysql'
+  host     : '193.203.168.98',
+  user     : 'u471236973_expense',
+  password : 'kalunga1998@F',
+  //port : '8889',
+  database : 'u471236973_expense'
 });
 
 export default connection;
