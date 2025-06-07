b39d340a79f020ece111c82175c9a3a3e0fa8c7a
francksefu <francksefu1998@gmail.com>
2025-06-07 10:15:49 +0200

    Update vercel.json file and test

--- server/index.mjs
+++ server/index.mjs
@@ -9,13 +9,8 @@ import bcrypt from 'bcryptjs';
 //const PORT = process.env.PORT || 3001;
 let courantUser;
 const app = express();
-//app.use(cors());
+app.use(cors());
 app.use(express.json());
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
 const secretKey = 'kalunga';
 
 const authenticateToken = (req, res, next) => {
