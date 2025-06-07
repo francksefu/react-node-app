3fe14eeca72bb96ac76cca962b98f22560f916c8
francksefu <francksefu1998@gmail.com>
2025-06-07 09:48:39 +0200

    Update backend url

--- trackmoneyapp/src/context/expenses.js
+++ trackmoneyapp/src/context/expenses.js
@@ -8,7 +8,7 @@ const Provider = ({ children }) => {
     const [loadingAllExpenses, setLoadingAllExpenses] = useState(true);
     const [expensesAll, setExpensesAll] = useState([]);
     //let port = '3001';
-    let baseUrl = 'https://react-node-app-psi.vercel.app';
+    let baseUrl = 'https://react-node-app-3fyn.vercel.app';
     const getExpenses = async () => {
         const url = `${baseUrl}/expenses`;
         setLoading(true);
